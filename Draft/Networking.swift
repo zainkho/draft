097875d8aecd91d8b9f7e16e70ae9ba74323ab48
William Ma <whoiswillma@gmail.com>
2019-11-23 21:29:03 -0500

    Convert Networking to https

--- Draft/Networking.swift
+++ Draft/Networking.swift
@@ -9,25 +9,46 @@ import Foundation
 
 final class Networking {
 
+    enum Error: Swift.Error {
+        case backendError(String)
+
+        var localizedDescription: String {
+            switch self {
+            case .backendError(let error): return error
+            }
+        }
+    }
+
     struct Entry {
 
         let id: Int
         let description: String
-        let kind: String
         let dayIndex: Int
+
+        let kind: String?
+        let completed: Bool
+
+        init(id: Int, description: String, dayIndex: Int, kind: String?, completed: Bool) {
+            self.id = id
+            self.description = description
+            self.dayIndex = dayIndex
+            self.kind = kind
+            self.completed = completed
+        }
 
         init?(json: [String: Any]) {
             guard let id = json["id"] as? Int,
                 let description = json["description"] as? String,
-                let kind = json["kind"] as? String,
                 let dayIndex = json["day_index"] as? Int else {
                     return nil
             }
 
             self.id = id
             self.description = description
-            self.kind = kind
             self.dayIndex = dayIndex
+
+            self.kind = json["kind"] as? String
+            self.completed = json["completed"] as? Bool ?? false
         }
 
     }
@@ -37,6 +58,17 @@ final class Networking {
         let id: Int
         let name: String
         let entries: [Entry]
+
+        let location: String?
+        let imageUrl: URL?
+
+        init(id: Int, name: String, entries: [Entry], location: String) {
+            self.id = id
+            self.name = name
+            self.entries = entries
+            self.location = location
+            self.imageUrl = nil
+        }
 
         init?(json: [String: Any]) {
             guard let id = json["id"] as? Int,
@@ -48,6 +80,9 @@ final class Networking {
             self.id = id
             self.name = name
             self.entries = entriesData.compactMap(Entry.init)
+
+            self.location = json["location"] as? String
+            self.imageUrl = (json["image_url"] as? String).flatMap { URL(string: $0) }
         }
 
     }
@@ -74,28 +109,39 @@ final class Networking {
 
     static let shared = Networking()
 
-    func getUsers(_ completion: @escaping ([User]) -> Void) {
-        guard let usersUrl = URL(string: "http://localhost:5000/api/users/") else {
-            completion([])
+    func getUsers(_ completion: @escaping (Result<[User], Swift.Error>) -> Void) {
+        guard let usersUrl = URL(string: "https://draft-backend.duckdns.org/api/users/") else {
+            completion(.success([]))
             return
         }
 
         let task = URLSession.shared.dataTask(with: usersUrl) { (data, response, error) in
             guard let data = data, error == nil else {
-                print(error)
+                completion(error.map { .failure($0) } ?? .success([]))
                 return
             }
 
-            let results = String(data: data, encoding: .utf8)
-            let response = try! JSONSerialization.jsonObject(with: data) as! [String: Any]
-            let users = (response["data"] as! [[String: Any]]).compactMap(User.init)
-            print(users)
+            do {
+                guard let response = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    completion(.success([]))
+                    return
+                }
+
+                if let success = response["success"] as? Bool, success,
+                    let usersArray = response["data"] as? [[String: Any]] {
+                    completion(.success(usersArray.compactMap(User.init)))
+                } else if let error = response["error"] as? String {
+                    completion(.failure(Error.backendError(error)))
+                }
+            } catch {
+                completion(.failure(error))
+            }
         }
 
         task.resume()
     }
 
-    init() {
+    private init() {
     }
 
 }
